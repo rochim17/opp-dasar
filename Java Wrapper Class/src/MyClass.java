8791abc6ba698b74a76bfd339f44c4497eaf70d7
rochim17 <56856426+rochim17@users.noreply.github.com>
2020-01-13 20:24:26 +0700

    Add files via upload

---
+++ Java Wrapper Class/src/MyClass.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author nima
+ */
+public class MyClass {
+  public static void main(String[] args) {
+    Integer myInt = 5;
+    Double myDouble = 5.99;
+    Character myChar = 'A';
+    System.out.println(myInt);
+    System.out.println(myDouble);
+    System.out.println(myChar);
+  }
+}
