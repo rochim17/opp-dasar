2b438691c19806f483b6e00b16ec6beb9b592b70
rochim17 <56856426+rochim17@users.noreply.github.com>
2020-01-13 20:25:32 +0700

    Add files via upload

---
+++ Java Exceptions/src/JavaTryAndCatch/Syntax.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package JavaTryAndCatch;
+
+/**
+ *
+ * @author nima
+ */
+public class Syntax {
+  public static void main(String[ ] args) {
+    int[] myNumbers = {1, 2, 3};
+    System.out.println(myNumbers[10]); // error!
+  }
+}
+//terjadi output akan error karena angka 10 dalam data tidak tersedia
+
---
+++ Java Exceptions/src/TheThrowKeywords/MyClass.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TheThrowKeywords;
+
+/**
+ *
+ * @author nima
+ */
+public class MyClass {
+  static void checkAge(int age) {
+    if (age < 18) {
+      throw new ArithmeticException("Access denied - You must be at least 18 years old.");
+    }
+    else {
+      System.out.println("Access granted - You are old enough!");
+    }
+  }
+
+  public static void main(String[] args) {
+    //checkAge(15); 
+    checkAge(20);
+  }}
+
