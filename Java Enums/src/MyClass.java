a61a12ca6388ff5f646ad97e342bb954994676e6
rochim17 <56856426+rochim17@users.noreply.github.com>
2020-01-13 20:19:20 +0700

    Add files via upload

---
+++ Java Enums/src/MyClass.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author nima
+ */
+public class MyClass {
+  enum Level {
+    LOW,
+    MEDIUM,
+    HIGH
+  }
+
+  public static void main(String[] args) {
+    Level myVar = Level.MEDIUM; 
+    System.out.println(myVar);
+  }
+}
