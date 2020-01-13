37745ea5ea267089af55486d8cd616ce7978ca88
rochim17 <56856426+rochim17@users.noreply.github.com>
2020-01-13 20:23:54 +0700

    Add files via upload

---
+++ Java HashMap/src/MyClass.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.util.HashMap;
+/**
+ *
+ * @author nima
+ */
+// Import the HashMap class
+
+
+public class MyClass {
+  public static void main(String[] args) {
+    // Create a HashMap object called capitalCities
+    HashMap<String, String> capitalCities = new HashMap<String, String>();
+
+    // Add keys and values (Country, City)
+    capitalCities.put("England", "London");
+    capitalCities.put("Germany", "Berlin");
+    capitalCities.put("Norway", "Oslo");
+    capitalCities.put("USA", "Washington DC");
+    System.out.println(capitalCities);
+  }
+}
